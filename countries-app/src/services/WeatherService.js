93f70e92425d7688d6dec1ab5c991645a45412f3
Alexso47 <alekox97@gmail.com>
2021-02-25 19:31:21 +0100

    fix

--- countries-app/src/services/WeatherService.js
+++ countries-app/src/services/WeatherService.js
@@ -4,6 +4,5 @@ export const getCountryWeather = async (city, key) => {
     const url = 'http://api.weatherstack.com/current?access_key=' + key + '&query=' + city
     console.log(url)
     const response = await axios.get(url)
-    console.log(response)
     return response.data
 }
