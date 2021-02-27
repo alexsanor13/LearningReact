c66f9ff4ad65a0cc7497bb927dbbd72dcd876064
Alexso47 <alekox97@gmail.com>
2021-02-27 18:16:09 +0100

    created backend with dependences

--- part3-backend/index.js
+++ part3-backend/index.js
@@ -1,32 +1,99 @@
-const http = require('http')
+const express = require('express')
+const app = express()
+const logger = require('./loggerMiddleware')
 
-const notes = [
+app.use(express.json())
+
+app.use(logger)
+
+let notes = [
     {
-      id: 1,
-      content: 'HTML is easy',
-      date: '2019-05-30T17:30:31.098Z',
-      important: true
+        id: 1,
+        content: 'HTML is easy',
+        date: '2019-05-30T17:30:31.098Z',
+        important: true
     },
     {
-      id: 2,
-      content: 'Browser can execute only JavaScript',
-      date: '2019-05-30T18:39:34.091Z',
-      important: false
+        id: 2,
+        content: 'Browser can execute only JavaScript',
+        date: '2019-05-30T18:39:34.091Z',
+        important: false
     },
     {
-      id: 3,
-      content: 'GET and POST are the most important methods of HTTP protocol',
-      date: '2019-05-30T19:20:16.298Z',
-      important: true
+        id: 3,
+        content: 'GET and POST are the most important methods of HTTP protocol',
+        date: '2019-05-30T19:20:16.298Z',
+        important: true
     }
-  ]
+]
 
+app.get('/', (request, response) => {
+    response.send('<h1>Hello World</h1>')
+})
 
-const app = http.createServer((request, response) => {
-    response.writeHead(200, { 'Content-type': 'application/json '})
-    response.end(JSON.stringify(notes))
+app.get('/api/notes', (request, response) => {
+    response.json(notes)
+})
+ 
+app.get('/api/notes/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const note = notes.find(n => n.id === id)
+    if(note) {
+        response.json(note)
+    }
+    else {
+        response.status(404).end()
+    }
+})
+
+app.delete('/api/notes/:id', (request, response) => {
+    const id = Number(request.params.id)
+    notes = notes.filter(n => n.id !== id)
+    response.status(204).end()
+})
+
+app.post('/api/notes', (request, response) => {
+    const note = request.body
+    
+    if(!note || !note.content) {
+        return response.status(400).json({
+            error: 'note.content is missing'
+        })
+    }
+
+    const ids = notes.map(n => n.id)
+    let maxId = Math.max(...ids)
+
+    const newNote = {
+        id: maxId + 1,
+        content: note.content,
+        date: new Date().toISOString(),
+        important: note.important !== undefined ? note.important : false
+    } 
+    notes = [...notes, newNote]
+    response.status(201).json(newNote)
+})
+
+app.use((request, response) => {
+    console.log(request.path)
+    
+    response.status(404).json({
+        error: 'Not found'
+    })
 })
 
 const PORT = 3001
-app.listen(PORT)
-console.log('Server running on port', PORT)
+// te aseguras que el servidor está levantado antes de ejecutar el console.log
+app.listen(PORT, () => {
+    console.log('Server running on port', PORT)
+})
+
+// const http = require('http')
+// const app = http.createServer((request, response) => {
+//     response.writeHead(200, { 'Content-type': 'application/json '})
+//     response.end(JSON.stringify(notes))
+// })
+
+// app.listen(PORT)
+// console.log('Server running on port', PORT)
+
